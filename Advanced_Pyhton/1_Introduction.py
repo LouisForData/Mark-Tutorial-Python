3f791a20444e5353337a10a26fa8d0e2dfdf1b7d
LouisForData <louis.k.mwangi@gmail.com>
2024-12-05 20:54:33 +0300

    first commit

---
+++ Advanced_Pyhton/1_Introduction.py
@@ -0,0 +1,26 @@
+"""
+print("Hello world")
+print()
+print("Mark")
+print()
+print("This is my first time to learn python")
+
+print()
+"""
+"""
+first_number = int(input("Please enter the first number:  "))
+second_number = int(input("Please enter second number:  "))
+
+x = first_number + second_number
+
+
+print(x)
+"""
+
+first_number = input("Please enter the first number:  ")
+second_number = input("Please enter second number:  ")
+
+x = int(first_number) + int(second_number)
+
+
+print(x)
